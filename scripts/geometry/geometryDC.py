320969cab959ba854fe30432035c5f1962273b8f
Gagik Gavalian <gavalian@Gagiks-MacBook-Air.local>
2014-07-11 11:09:06 -0400

    added dc test class

--- scripts/geometry/geometryDC.py
+++ scripts/geometry/geometryDC.py
@@ -2,7 +2,7 @@
 # FULL geometry test
 #===================================================================
 from org.jlab.detector.base import DetectorDescriptor
-from org.jlab.detector.ec   import ECDetectorFactory, ECLayerFactory
+from org.jlab.detector.dch  import DCDetectorFactory, DCLayerFactory
 from org.jlab.clas12.dbdata import DataBaseLoader
 from org.jlab.geom  import Path3D,Face3D,Line3D,Point3D,Transformation3D
 import random
@@ -13,31 +13,40 @@ from Particle import Particle
 # 
 #===================================================================
 
-dataProvider = DataBaseLoader.getCalorimeterConstants()
+dataProvider = DataBaseLoader.getDriftChamberConstants()
 print dataProvider.toString()
 
-ecDetector   = ECDetectorFactory.createDetector(dataProvider)
-ecDetector.show()
+dcDetector   = DCDetectorFactory.createDetector(dataProvider)
+dcDetector.show()
 
 print '\n\n\n------>'
 
-ecLayerU     = ECLayerFactory.createLayer(dataProvider,0,0,0)
-ecLayerU.show()
-ecLayerV     = ECLayerFactory.createLayer(dataProvider,0,0,1)
-ecLayerV.show()
-ecLayerW     = ECLayerFactory.createLayer(dataProvider,0,0,2)
-ecLayerW.show()
+for s in range(0,6):
+    print 'super Layer = ',s
+    for i in range(0,6):
+        dcLayerU     = DCLayerFactory.createLayer(dataProvider,0,s,i)
+        dcLayerU.show()
 
+#ecLayerV     = ECLayerFactory.createLayer(dataProvider,0,0,1)
+#ecLayerV.show()
+#ecLayerW     = ECLayerFactory.createLayer(dataProvider,0,0,2)
+#ecLayerW.show()
+
+dcLayerU     = DCLayerFactory.createLayer(dataProvider,0,0,0)
+for i in range(0,112):
+    line = dcLayerU.getLine(i)
+    print i,line.origin().x(),line.origin().y(),line.end().x(),line.end().y()
 
 path = Path3D()
 
 for i in range(0,10000):
     path.generateRandom(0.0,0.0,0.0,10.0,35.0,-180.,180.,15000.0,6)
-    hits = ecDetector.getLayerHits(path)
+    hits = dcDetector.getLayerHits(path)
     #print '-------------------------> EVENT ',i
     for hit in hits:
         #print hit.toString()
-        print hit.getDescriptor().getLayer(),hit.getDescriptor().getComponent(),hit.position().x(),hit.position().y(),hit.position().z()
+        print hit.getDescriptor().getSuperLayer(),hit.getDescriptor().getLayer(),hit.getDescriptor().getComponent(),
+        print hit.position().x(),hit.position().y(),hit.position().z()
 
 
 #surf = ecLayerV.getBoundary()
