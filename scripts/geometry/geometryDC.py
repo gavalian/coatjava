c1ad60f65ce14b22c209bcf9c306c2b584c7e282
Gagik Gavalian <gavalian@meson.jlab.org>
2014-07-15 10:59:39 -0400

    updated geometry lib

--- scripts/geometry/geometryDC.py
+++ scripts/geometry/geometryDC.py
@@ -14,10 +14,11 @@ from Particle import Particle
 #===================================================================
 
 dataProvider = DataBaseLoader.getDriftChamberConstants()
-print dataProvider.toString()
+#print dataProvider.toString()
 
 dcDetector   = DCDetectorFactory.createDetector(dataProvider)
-dcDetector.show()
+#dcDetector.getLayer(0,0,0).show()
+#dcDetector.show()
 
 print '\n\n\n------>'
 
@@ -35,17 +36,17 @@ for s in range(0,6):
 dcLayerU     = DCLayerFactory.createLayer(dataProvider,0,0,0)
 for i in range(0,112):
     line = dcLayerU.getLine(i)
-    print i,line.origin().x(),line.origin().y(),line.end().x(),line.end().y()
+    #print i,line.origin().x(),line.origin().y(),line.end().x(),line.end().y()
 
 path = Path3D()
 
-for i in range(0,10000):
+for i in range(0,10):
     path.generateRandom(0.0,0.0,0.0,10.0,35.0,-180.,180.,15000.0,6)
     hits = dcDetector.getLayerHits(path)
-    #print '-------------------------> EVENT ',i
+    #print '-------------------------> EVENT ',i, hits.size()
     for hit in hits:
         #print hit.toString()
-        print hit.getDescriptor().getSuperLayer(),hit.getDescriptor().getLayer(),hit.getDescriptor().getComponent(),
+        print hit.getDescriptor().getSector(),hit.getDescriptor().getSuperLayer(),hit.getDescriptor().getLayer(),hit.getDescriptor().getComponent(),
         print hit.position().x(),hit.position().y(),hit.position().z()
 
 
