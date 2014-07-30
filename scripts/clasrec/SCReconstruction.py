d3ef12bba0f2f0c73dfb00443e0ab38508d7de7f
Gagik Gavalian <gavalian@meson.jlab.org>
2014-07-30 16:45:55 -0400

    clas-physics.jar is added to the library. clasrec-main updated

---
+++ scripts/clasrec/SCReconstruction.py
@@ -0,0 +1,44 @@
+#=====================================================================
+# A python module that implements a Java interface to
+# create a building object
+#=====================================================================
+import sys
+from java.lang         import System
+from java.lang         import String
+from  org.jlab.evio.clas12  import EvioDataDictionary
+from  org.jlab.evio.clas12  import EvioFactory
+from  org.jlab.evio.clas12  import EvioSource
+from  org.jlab.evio.clas12  import EvioDataBank
+from  org.jlab.evio.clas12  import EvioDataSync
+from  org.jlab.rec.sc       import SCReconstruction
+from  org.jlab.clas12.seb   import SEBEventBuilderDebug
+
+#-----------------------------------------------------------
+# Initilizing EvioSource object. It in turn initializes 
+# EvioFactory which loads the dictionary from directory
+# CLAS12DIR/lib/bankdefs/clas12
+#-----------------------------------------------------------
+
+inputFile = sys.argv[1]
+
+writer = EvioDataSync()
+writer.open('sc_reconstruction_output.evio')
+
+reader = EvioSource()
+reader.open(inputFile)
+
+ftofProc = SCReconstruction()
+ftofProc.init()
+
+builder = SEBEventBuilderDebug()
+builder.init()
+
+icounter = 0
+while(reader.hasEvent()):
+    event = reader.getNextEvent()
+    builder.processEvent(event)
+    ftofProc.processEvent(event)
+    writer.writeEvent(event)
+
+print 'Events analyzed from File = ', icounter
+writer.close()
