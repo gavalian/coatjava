4d9a2e3a86c2d50f6058d7d1579af7165c0ff6df
Gagik Gavalian <gavalian@meson.jlab.org>
2014-08-12 10:34:37 -0400

    BST factory now creates rings

--- scripts/visualizer/clasViewer.py
+++ scripts/visualizer/clasViewer.py
@@ -2,6 +2,8 @@
 # FULL geometry test
 #===================================================================
 from org.jlab.geom.visualizer    import CLASVisualizer
+from org.jlab.geom.detector.bst  import BSTFactory
+from org.jlab.clas12.dbdata import DataBaseLoader
 from org.jlab.geom.prim  import Path3D,Face3D,Line3D,Point3D,Transformation3D
 from java.lang import Math
 from java.awt import Color
@@ -13,6 +15,12 @@ import sys
 # 
 #===================================================================
 
+data = DataBaseLoader.getConstantsBST()
+print data.toString()
+
+factory = BSTFactory()
+
+
 viewer = CLASVisualizer()
 viewer.setVisible(True)
 
@@ -20,5 +28,23 @@ path = Path3D()
 path.addPoint(0.0,0.0,0.0)
 path.addPoint(300.0,400.0,200.0)
 
-viewer.add(path,Color.WHITE)
+#viewer.add(path,Color.WHITE)
+#for i in range(0,10):
+#    layer   = factory.createRingLayer(data,0,0,i)
+#    viewer.add(layer,Color(10,50,100))
+#    layer   = factory.createRingLayer(data,0,1,i)
+#    viewer.add(layer,Color(25,125,250))
+
+#for i in range(0,10):
+#    layer   = factory.createRingLayer(data,1,0,i)
+#    viewer.add(layer,Color(100,200,100))
+#    layer   = factory.createRingLayer(data,1,1,i)
+#    viewer.add(layer,Color(50,100,50))
+
+ring = factory.createRing(data,0)
+viewer.add(ring,Color(100,100,200))
+
+ring = factory.createRing(data,1)
+viewer.add(ring,Color(100,200,100))
+
 viewer.update()
