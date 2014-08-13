f72b6d7919b376f55156373977b38ce74136b8b9
Gagik Gavalian <gavalian@meson.jlab.org>
2014-08-13 15:12:05 -0400

    modified BST geometry to match GEMC

--- scripts/visualizer/clasViewer.py
+++ scripts/visualizer/clasViewer.py
@@ -21,7 +21,9 @@ print data.toString()
 factory = BSTFactory()
 
 
-viewer = CLASVisualizer()
+viewer = CLASVisualizer(100,100,1400,1200)
+viewer.setBackgroundColor(Color(150,150,150))
+viewer.getDisplay().setBackgroundColor(Color(150,150,150))
 viewer.setVisible(True)
 
 path = Path3D()
@@ -29,11 +31,11 @@ path.addPoint(0.0,0.0,0.0)
 path.addPoint(300.0,400.0,200.0)
 
 #viewer.add(path,Color.WHITE)
-#for i in range(0,10):
-#    layer   = factory.createRingLayer(data,0,0,i)
-#    viewer.add(layer,Color(10,50,100))
-#    layer   = factory.createRingLayer(data,0,1,i)
-#    viewer.add(layer,Color(25,125,250))
+for i in range(0,1):
+    layer   = factory.createRingLayer(data,0,0,i)
+    viewer.add(layer,Color(10,50,100))
+    layer   = factory.createRingLayer(data,0,1,i)
+    viewer.add(layer,Color(25,125,250))
 
 #for i in range(0,10):
 #    layer   = factory.createRingLayer(data,1,0,i)
@@ -41,10 +43,10 @@ path.addPoint(300.0,400.0,200.0)
 #    layer   = factory.createRingLayer(data,1,1,i)
 #    viewer.add(layer,Color(50,100,50))
 
-ring = factory.createRing(data,0)
-viewer.add(ring,Color(100,100,200))
+#ring = factory.createRing(data,0)
+#viewer.add(ring,Color(100,100,200))
 
-ring = factory.createRing(data,1)
-viewer.add(ring,Color(100,200,100))
+#ring = factory.createRing(data,1)
+#viewer.add(ring,Color(100,200,100))
 
 viewer.update()
